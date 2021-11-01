f1168f6507aa782c88d89655d6f6ab4a5dbda453
Sheikhasib <sheikhhasib037@gmail.com>
2021-11-01 23:50:15 +0600

    done

--- src/Pages/Home/TourSpot/TourSpot.js
+++ src/Pages/Home/TourSpot/TourSpot.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import "./TourSpot.css";
 
-const TourSpot = ({spot}) => {
-    const {_id, name, description, img} = spot;
-    return (
-        <div className="spot pb-5">
-            <img src={img} alt="" />
+const TourSpot = ({ spot }) => {
+  const { _id, name, description, img } = spot;
+  return (
+    <div className="spot pb-5">
+      <img src={img} alt="" />
       <h3>{name}</h3>
       <p>{description}</p>
       <Link to={`/booking/${_id}`}>
         <button className="btn btn-info">Booking</button>
       </Link>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default TourSpot;
